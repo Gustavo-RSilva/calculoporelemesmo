05765d88778c65f59a0f6f4d591cd08d8a199ca6
Gustavo.RSilva <gu222498@gmail.com>
2024-11-18 09:41:10 -0300

    Adicionar arquivos de projeto.

---
+++ calculoporelemesmo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculoporelemesmo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int N1 = int.Parse(textBox1.Text), calculo = 0;
+            for (int i = 1; i <= N1; i++)
+            {
+                calculo += i ;
+                
+            }
+            listBox1.Items.Add(calculo);
+        }
+    }
+}
